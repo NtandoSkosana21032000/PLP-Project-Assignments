70d3e056dd51218523de84360547a928ac9ec98b
NtandoSkosana21032000 <160571519+NtandoSkosana21032000@users.noreply.github.com>
2024-03-27 21:48:01 +0200

    Add files via upload

---
+++ My_List.py
@@ -0,0 +1,33 @@
+#Empty List
+My_List = []
+print("My_List:", My_List)
+
+#Append
+My_List.append(10,)
+My_List.append(20,)
+My_List.append(30,)
+My_List.append(40,)
+print("appending_My_List", My_List)
+
+#Insert
+My_List.insert(1, "15")
+print(My_List)
+
+#extend
+My_List_2 = [50, 60, 70]
+My_List.extend(My_List_2)
+print("Extending_My_List", My_List)
+
+#Remove
+del My_List[7]
+print("removal", My_List)
+
+#sort 
+My_List.sort
+print("Ascending_order", My_List)
+
+#index
+print(My_List[3])
+
+
+
